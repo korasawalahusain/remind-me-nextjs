9a6a5ed7195a49367d20d9068d054097e1ee5390
Husain Korasawala <korasawala.husain@gmail.com>
2024-08-16 02:54:31 +0530

    [fix] logo text truncate

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 export default function Logo({}: Props) {
   return (
-    <h1 className="text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
+    <h1 className="text-2xl truncate font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
       Husain K&apos;s RemindMe
     </h1>
   );
